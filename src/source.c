e496e0c9883a065e972f7768d36da58b42ac8dc7
Jürgen Fredriksson <jiargei@gmail.com>
2024-02-02 12:24:23 +0100

    wip-2024033-1224

--- src/source.c
+++ src/source.c
@@ -900,6 +900,7 @@ source_write_to_client (source_t *source, connection_t *clicon)
 
   if (client->virgin == CLIENT_PAUSED || client->virgin == -1)
     {
+      printf ("DONT SEND, PAUSING");
       xa_debug (1, "WARNING: client paused");
       return;
     }
